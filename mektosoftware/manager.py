58f3c459a8b03bcc7cf33fbc40371e6d3a2b9ff9
Tomasz Krzyszczyk <tomasz.krzyszczyk@gmail.com>
2012-08-26 23:47:30 +0200

    Move `published on` date to post filename

--- mektosoftware/manager.py
+++ mektosoftware/manager.py
@@ -24,11 +24,20 @@ class PostManager(list):
         del self[:]
 
     def find_posts(self):
+        """Finds and reads templates in `/posts` directory and extracts info about them.
+        The template filenames shoud be in `YYMMDD-slug.html` format.
+        """
+
         self.clear()
 
         for path in self.app.jinja_env.list_templates(filter_func=lambda t: t.startswith('posts/') and t.endswith('.html')):
             template = self.app.jinja_env.get_template(path)
-            published_on = datetime.strptime(getattr(template.module, 'published_on', str(date.today())), '%Y-%m-%d').date()
-            self.append(Post(title=template.module.title, slug=path[6:-5], published_on=published_on, path=path))
+
+            filename = path[6:-5]
+            slug = filename[7:]
+            date_fragment = filename[0:6]
+            published_on = datetime.strptime(date_fragment, '%y%m%d').date()
+
+            self.append(Post(title=template.module.title, slug=slug, published_on=published_on, path=path))
 
         self.sort(key=lambda post: post.published_on, reverse=True)
