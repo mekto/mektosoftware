ea057cc707c5c310faac877e319ba7947737562c
Tomasz Krzyszczyk <tomasz.krzyszczyk@gmail.com>
2012-08-29 22:20:35 +0200

    Add commands for freezing the app

--- manage.py
+++ manage.py
@@ -1,8 +1,21 @@
 from flask.ext.script import Manager
+from flask.ext.frozen import Freezer
 from mektosoftware import app
 
 
 manager = Manager(app)
+
+@manager.command
+def freeze():
+    """ Freezes application into a set of static files. """
+    freezer = Freezer(app)
+    freezer.freeze()
+
+@manager.command
+def freeze_test():
+    """ Freezes application and starts an HTTP server to test the build result. """
+    freezer = Freezer(app)
+    freezer.run()
 
 if __name__ == '__main__':
     manager.run()
